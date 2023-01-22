321597122be33bd0e689e29ffc27a1d7c2037676
msm-code <msm@tailcall.net>
2023-01-22 04:15:53 +0100

    feat: add a run command, and small libvirt scaffolding (#2)

---
+++ vixos/libvirt_utils.py
@@ -0,0 +1,15 @@
+import libvirt
+from contextlib import contextmanager
+from typing import Any
+
+
+@contextmanager
+def libvirt_connection(uri: str) -> Any:
+    conn = libvirt.open(uri)
+    if not conn:
+        raise RuntimeError(f"Failed to open connection to {uri}")
+
+    try:
+        yield conn
+    finally:
+        conn.close()
