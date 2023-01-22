cd996947a97cc6b9c6e17729bb73db6a84a4496c
msm-code <msm@tailcall.net>
2023-01-22 03:50:02 +0100

    feat: add initial project template (#1)

---
+++ README.md
@@ -0,0 +1,3 @@
+# VixOS
+
+VixOS is a secure app launcher, inspired by QubesOS and jollheef/appvm.
---
+++ vixos/main.py
@@ -0,0 +1,8 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="VixOS is a secure application launcher.")
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
