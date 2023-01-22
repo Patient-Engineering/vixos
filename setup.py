082b5996e496899cb7b5e6b74575d382e2e444ba
msm <msm@tailcall.net>
2023-01-22 03:49:18 +0100

    feat: add initial project template

---
+++ setup.py
@@ -0,0 +1,30 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="vixos",
+    version="0.0.0",
+    description="VixOS is a secure app launcher, inspired by QubesOS and jollheef/appvm.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Patient Engineering",
+    author_email="msm@tailcall.net",
+    packages=["vixos"],
+    entry_points={
+        "console_scripts": [
+            "vixos = vixos.main:main",
+        ],
+    },
+    license="GPLv3",
+    include_package_data=True,
+    install_requires=open("requirements.txt").read().splitlines(),
+    url="https://github.com/Patient-Engineering/vixos",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.7'
+)
+
