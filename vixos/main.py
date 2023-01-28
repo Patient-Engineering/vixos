9592f57162caa12860dd7b5bf77e55a6facc0086
msm <msm@tailcall.net>
2023-01-28 16:48:10 +0100

    feat: make it possible to run a basic program in a vm

--- vixos/main.py
+++ vixos/main.py
@@ -1,12 +1,97 @@
+import re
+import os
 import argparse
+import subprocess
+from pathlib import Path
+from .template_xml import generate_xml
+from .template_nix import generate_base_nix, generate_nix
 from .libvirt_utils import libvirt_connection
+from typing import Tuple
+
+
+class AppVM:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def xml_config(
+        self, vmname: str, vm_path: str, reginfo: str, image_path: str, shared_path: str
+    ) -> str:
+        return generate_xml(
+            vmname=vmname,
+            network="libvirt",
+            gui=True,
+            vm_path=vm_path,
+            reginfo=reginfo,
+            image_path=image_path,
+            shared_dir=shared_path,
+        )
+
+    def make_nix_config_file(self, vixos_path: str) -> str:
+        with open(f"{vixos_path}/base.nix", "w") as configf:
+            configf.write(generate_base_nix())
+
+        configfile = f"{self.name}.nix"
+        configpath = f"{vixos_path}/{configfile}"
+        with open(configpath, "w") as configf:
+            configf.write(generate_nix(self.name))
+        return configfile
+
+    def generate_vm(self, vixos_path: str, name: str) -> Tuple[str, str, str]:
+        subprocess.check_call(
+            [
+                "nix-build",
+                "<nixpkgs/nixos>",
+                "-A",
+                "config.system.build.vm",
+                "-I",
+                f"nixos-config={vixos_path}/{name}",
+                "-I",
+                vixos_path,
+            ]
+        )
+
+        with open("result/bin/run-nixos-vm", "r") as configf:
+            config = configf.read()
+
+        reginfo = re.findall("regInfo=.*/registration", config)[0]
+        print("reginfo=", reginfo)
+
+        realpath = os.readlink("result/system")
+        print("realpath=", realpath)
+        os.unlink("result")
+
+        qcow2 = f"{vixos_path}/{name}.fake.qcow2"
+        if not Path(qcow2).exists():
+            subprocess.check_call(["qemu-img", "create", "-f", "qcow2", qcow2, "40M"])
+
+        return (realpath, reginfo, qcow2)
+
+    def start(self, conn) -> None:
+        vixos_path = os.path.expanduser("~/vixos")
+        Path(vixos_path).mkdir(exist_ok=True)
+
+        shared_path = vixos_path + "/shared"
+        Path(shared_path).mkdir(exist_ok=True)
+
+        vmname = f"vixos_{self.name}"
+
+        config_file = self.make_nix_config_file(vixos_path)
+        vm_path, reginfo, qcow2 = self.generate_vm(vixos_path, config_file)
+        config = self.xml_config(vmname, vm_path, reginfo, qcow2, shared_path)
+        dom = conn.createXML(config)
+        if not dom:
+            raise SystemExit("Failed to create a domain from an XML definition")
+
+        print("Guest " + dom.name() + " has booted")
 
 
 def run(args):
     print(f"OK, running {args.package}...")
 
+    appvm = AppVM(args.package)
+
     with libvirt_connection("qemu:///system") as conn:
-        print(conn.getCapabilities())
+        appvm.start(conn)
 
 
 def main():
@@ -20,6 +105,11 @@ def main():
     run_parser.add_argument(
         "package",
         help="Name of the nixpkgs package to run.",
+    )
+    run_parser.add_argument(
+        "--gui",
+        help="If specified, add graphical devices to the VM.",
+        action="store_true",
     )
 
     args = parser.parse_args()
---
+++ vixos/template_nix.py
@@ -0,0 +1,146 @@
+import os
+
+
+def generate_nix(package: str) -> str:
+    return """{pkgs, ...}:
+let
+  application = "${pkgs.%s}/bin/%s";
+  appRunner = pkgs.writeShellScriptBin "app" ''
+    ARGS_FILE=/home/user/.args
+    ARGS=$(cat $ARGS_FILE)
+    rm $ARGS_FILE
+
+    ${application} $ARGS
+    systemctl poweroff
+  '';
+in {
+  imports = [
+    <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix>
+    <base.nix>
+  ];
+
+  environment.systemPackages = [ appRunner pkgs.%s ];
+
+  services.xserver.displayManager.sessionCommands = "${appRunner}/bin/app &";
+}
+""" % (
+        package,
+        package,
+        package,
+    )
+
+
+def generate_base_nix():
+    uid = os.getuid()
+    return base_nix % (uid)
+
+
+base_nix = """{pkgs, ...}:
+{
+  services.xserver = {
+    enable = true;
+    desktopManager.xterm.enable = false;
+    displayManager = {
+      lightdm.enable = true;
+      autoLogin = {
+        enable = true;
+        user = "user";
+      };
+    };
+    windowManager.xmonad.enable = true;
+  };
+
+  services.spice-vdagentd.enable = true;
+
+  users.extraUsers.user = {
+    uid = %s;
+    isNormalUser = true;
+    extraGroups = [ "audio" ];
+    createHome = true;
+    initialPassword = "user";
+  };
+
+  environment.etc."xmonad.hs".text = ''
+import XMonad
+main = xmonad defaultConfig
+  { workspaces = [ "" ]
+  , borderWidth = 0
+  , startupHook = startup
+  }
+
+startup :: X ()
+startup = do
+  spawn "while [ 1 ]; do ${pkgs.spice-vdagent}/bin/spice-vdagent -x; done &"
+  '';
+
+  systemd.services.home-user-build-xmonad = {
+    description = "Link xmonad configuration";
+    serviceConfig = {
+      ExecStart = "/bin/sh -c 'mkdir -p /home/user/.xmonad && ln -sf /etc/xmonad.hs /home/user/.xmonad/xmonad.hs && /run/current-system/sw/bin/xmonad --recompile'";
+      RemainAfterExit = "yes";
+      User = "user";
+      Restart = "on-failure";
+      TimeoutSec = 10;
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  systemd.services.mount-home-user = {
+    description = "Mount /home/user (crutch)";
+    serviceConfig = {
+      ExecStart = "/bin/sh -c '/run/current-system/sw/bin/mount -t 9p -o trans=virtio,version=9p2000.L home /home/user'";
+      RemainAfterExit = "yes";
+      Type = "oneshot";
+      User = "root";
+    };
+    wantedBy = [ "sysinit.target" ];
+  };
+
+  systemd.user.services."xrandr" = {
+    serviceConfig = {
+      StartLimitBurst = 100;
+    };
+    script = "${pkgs.xorg.xrandr}/bin/xrandr --output Virtual-1 --mode $(${pkgs.xorg.xrandr}/bin/xrandr | grep '   ' | head -n 2 | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $1 }')";
+  };
+
+  services.getty.autologinUser = "user";
+
+  systemd.services."serial-getty@ttyS0" = {
+    enable = true;
+    wantedBy = [ "getty.target" ];
+    serviceConfig.Restart = "always";
+  };
+
+  systemd.user.timers."xrandr" = {
+    description = "Auto update resolution crutch";
+    timerConfig = {
+      OnBootSec = "1s";
+      OnUnitInactiveSec = "1s";
+      Unit = "xrandr.service";
+      AccuracySec = "1us";
+    };
+    wantedBy = ["timers.target"];
+  };
+
+  systemd.services."autoballoon" = {
+    serviceConfig = {
+      StartLimitBurst = 100;
+    };
+    script = ''
+      ${pkgs.procps}/bin/free -m | grep Mem | \
+        ${pkgs.gawk}/bin/awk '{print $2 "-" $4}' | \
+        ${pkgs.bc}/bin/bc > /home/user/.memory_used
+    '';
+  };
+
+  systemd.timers."autoballoon" = {
+    description = "Auto update resolution crutch";
+    timerConfig = {
+      OnBootSec = "1s";
+      OnUnitInactiveSec = "1s";
+      Unit = "autoballoon.service";
+      AccuracySec = "1us";
+    };
+    wantedBy = ["timers.target"];
+  };
+}"""
