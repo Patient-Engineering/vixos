33a9f9b11fafb451a483458ea0e528b33ee6544b
Bonus <root@bonusplay.pl>
2023-01-30 20:02:15 +0100

    convert to flakes

--- default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-with pkgs;
-mkShell {
-  packages = [
-    python39Packages.libvirt
-    ((import ./virt-viewer-without-menu/default.nix) pkgs)
-  ];
-}
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "secure app launcher, inspired by QubesOS and appvm";
+  nixConfig.bash-prompt = "\[vixos\]$ ";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShell = pkgs.mkShell {
+            packages = with pkgs; [
+              python39Packages.libvirt
+              ((import ./virt-viewer-without-menu) pkgs)
+            ];
+          };
+        }
+      );
+}
